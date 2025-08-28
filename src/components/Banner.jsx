2a66be9a9169227b619fff5040065c14dcf6fc15
Vivek Kumar Yadav <vivek.yadav@vista.com>
2025-08-28 14:23:00 +0530

    adding banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <header>
+        <div>
+            <img src="/src/assets/GloboLogo.png" alt="logo" />
+        </div>
+    </header>
+  )
+}
+
+export default Banner
